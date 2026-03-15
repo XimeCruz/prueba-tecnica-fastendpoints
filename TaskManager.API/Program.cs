8e2e4d382961300607ccc40a9549869b09296103
XimeCruz <Xmenacruzt@gmail.com>
2026-03-15 01:57:47 -0400

    feat(api): configure FastEndpoints, Swagger and PostgreSQL
    - Wire up dependency injection in Program.cs
    - Add Swagger documentation
    - Add connection string in appsettings.json
    - Auto-migrate database on startup

---
+++ TaskManager.API/Program.cs
@@ -0,0 +1,36 @@
+//var builder = WebApplication.CreateBuilder(args);
+//var app = builder.Build();
+
+//app.MapGet("/", () => "Hello World!");
+
+//app.Run();
+
+
+
+using FastEndpoints;
+using FastEndpoints.Swagger;
+using TaskManager.Infrastructure.Extensions;
+using TaskManager.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddFastEndpoints();
+builder.Services.SwaggerDocument();
+builder.Services.AddInfrastructure(builder.Configuration.GetConnectionString("Default")!);
+
+var app = builder.Build();
+
+// Auto-migrate on startup
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
+app.UseFastEndpoints();
+app.UseSwaggerGen();
+
+app.Run();
+
+public partial class Program { }
